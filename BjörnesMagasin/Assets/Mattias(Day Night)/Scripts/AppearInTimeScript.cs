f33b1521992314b666815dfe9757a755e8700f20
Shadowsental <33833247+Shadowsental@users.noreply.github.com>
2018-10-09 13:19:01 +0200

    Let's do the time roll, yeah

---
+++ BjörnesMagasin/Assets/Mattias(Day Night)/Scripts/AppearInTimeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearInTimeScript : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject redBall;
+    DayNightScript dayNightScript;
+
+    void Start()
+    {
+        dayNightScript = GetComponent<DayNightScript>();
+        ball.SetActive(false);
+        redBall.SetActive(false);
+    }
+    void Update()
+    {
+        if(dayNightScript.timer >= 180)
+        {
+            ball.SetActive(true);
+        }
+        if(dayNightScript.timer <= 179)
+        {
+            ball.SetActive(false);
+        }
+        if (dayNightScript.timer >= 360)
+        {
+            ball.SetActive(false);
+        }
+        if(dayNightScript.days == 2)
+        {
+            redBall.SetActive(true);
+        }
+        if(dayNightScript.days == 3)
+        {
+            redBall.SetActive(false);
+        }
+    }
+}
---
+++ BjörnesMagasin/Assets/Mattias(Day Night)/Scripts/DayNightScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightScript : MonoBehaviour
+{
+
+    public Text text;
+    public Text dayText;
+    public Text timeText;
+    public float timer = 0;
+    public int days;
+    int maxDays = 31;
+    int months;
+    public enum TimeOfDay
+    {
+        DAY,
+        EVENING,
+        NIGHT,
+        MORNING
+    }
+
+    public TimeOfDay timeOfDay;
+
+    void Start()
+    {
+        text = text.GetComponent<Text>();
+        dayText = dayText.GetComponent<Text>();
+        timeText = timeText.GetComponent<Text>();
+        Debug.Log(timeOfDay);
+        timeOfDay = TimeOfDay.DAY;
+        days = 1;
+        
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        switch (timeOfDay)
+        {
+
+            case TimeOfDay.DAY:
+                break;
+            case TimeOfDay.EVENING:
+                break;
+            case TimeOfDay.NIGHT:
+                break;
+            case TimeOfDay.MORNING:
+                break;
+        }
+
+        int minutes = (int)timer / 60;
+        int seconds = (int)timer % 60;
+
+        text.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        dayText.text = ("Day ") + days.ToString();
+        timeText.text = timeOfDay.ToString();
+
+        if (timer >= 180)
+        {
+            timeOfDay = TimeOfDay.DAY;
+            Debug.Log("It's day");
+        }
+        if(timer >= 360)
+        {
+            timeOfDay = TimeOfDay.EVENING;
+            Debug.Log("It's evening");
+        }
+        if(timer >= 540)
+        {
+            timeOfDay = TimeOfDay.NIGHT;
+            Debug.Log("It's night");
+        }
+        if(timer <= 179)
+        {
+            timeOfDay = TimeOfDay.MORNING;
+            Debug.Log("It's morning");
+        }
+        if (timer >= 720)
+        {
+            timer = 0;
+            days += 1;
+        }
+        if (days == maxDays)
+        {
+            days = 0;
+        }
+    }
+}
