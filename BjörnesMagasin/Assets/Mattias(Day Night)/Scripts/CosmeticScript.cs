8cf66df1283390ed5b8fecbda72df67fdc6429b7
Shadowsental <33833247+Shadowsental@users.noreply.github.com>
2018-10-18 16:20:10 +0200

    Tempobar Fixed

--- BjörnesMagasin/Assets/Mattias(Day Night)/Scripts/CosmeticScript.cs
+++ BjörnesMagasin/Assets/Mattias(Day Night)/Scripts/CosmeticScript.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CosmeticScript : MonoBehaviour {
 
+    public Button cosmeticMask1;
+    public Button cosmeticMask2;
+    public Button cosmeticMask3;
     public GameObject sadMask;
     public GameObject hat;
-    //equip mask?
-    //equipp equipped mask?
 
 
 	// Use this for initialization
@@ -15,22 +17,35 @@ public class CosmeticScript : MonoBehaviour {
 
         sadMask.GetComponentInChildren<Renderer>().enabled = false;
         hat.GetComponentInChildren<Renderer>().enabled = true;
+        cosmeticMask1.gameObject.SetActive(false);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("w"))//&& sad mask equipped
+
+        if (Input.GetKeyDown("w"))
         {
-            sadMask.GetComponentInChildren<Renderer>().enabled = true;
-            hat.GetComponentInChildren<Renderer>().enabled = false;
+            cosmeticMask1.gameObject.SetActive(true);
         }
 
             if (Input.GetKeyDown("s"))
         {
+            cosmeticMask1.gameObject.SetActive(false);
+        }
+        cosmeticMask1.onClick.AddListener(PutOnMask);
+
+        if (Input.GetKeyDown("e"))
+        {
             sadMask.GetComponentInChildren<Renderer>().enabled = false;
             hat.GetComponentInChildren<Renderer>().enabled = true;
         }
-
+        
+    }
+    void PutOnMask()
+    {
+        print("click on");
+        sadMask.GetComponentInChildren<Renderer>().enabled = true;
+        hat.GetComponentInChildren<Renderer>().enabled = false;
     }
 }
