cb2b47fe8f66e13441c93cfd6617dd9d6742db0c
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-10-22 09:35:30 +0200

    revengeance

--- BjörnesMagasin/Assets/Mattias(Day Night)/Scripts/PowerbarMinigameScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PowerbarMinigameScript : MonoBehaviour {
-
-    public GameObject selector, rightCube, wrongCubes;
-
-}
