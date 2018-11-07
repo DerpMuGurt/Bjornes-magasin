53236cebf9f0690c0ad4f5e57ad47d9a8d9f9f35
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-11-07 09:49:50 +0100

    PickUps

---
+++ Björnes-Magasin-Revengeance/Assets/WilliamScripts/DontDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    static DontDestroy instans;
+
+    // Use this for initialization
+    void Start () {
+        //   DontDestroyOnLoad(gameObject);
+  //      DontDestroyOnLoad(this.gameObject);
+        if (instans)
+            Destroy(gameObject);
+        //annars behåll karaktären 
+        else
+        {
+            instans = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Björnes-Magasin-Revengeance/Assets/WilliamScripts/PickUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject guiObject;
+    public GameObject thisObject;
+    public GameObject objectToShow;
+    bool dontShowObject;
+
+
+    void Start()
+    {
+    
+        guiObject.SetActive(false);
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        guiObject.SetActive(true);
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+            {
+            if (guiObject.activeInHierarchy == true)
+            {
+                thisObject.SetActive(false);
+                objectToShow.SetActive(true);
+                guiObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        guiObject.SetActive(false);
+    }
+}
