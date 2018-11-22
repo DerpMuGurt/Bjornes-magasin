716198ca764ce6cdcefa80184d28ed9b3b82eda6
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-11-22 15:42:04 +0100

    dd

--- Björnes-Magasin-Revengeance/Assets/WilliamScripts/MainMenu.cs
+++ Björnes-Magasin-Revengeance/Assets/WilliamScripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour {
 
     public void LoadLevel(int sceneIndex)
 	{
-        SceneManager.LoadSceneAsync(sceneIndex);
+        SceneManager.LoadScene("TestDemo");
 
     }
 
--- Björnes-Magasin-Revengeance/Assets/WilliamScripts/PauseMenu.cs
+++ Björnes-Magasin-Revengeance/Assets/WilliamScripts/PauseMenu.cs
@@ -57,8 +57,8 @@ public class PauseMenu : MonoBehaviour {
 		Debug.Log ("Load Menu");
 		Time.timeScale = 1f;
 		GameIsPaused = false;
-		SceneManager.LoadScene (0);
-	}
+        SceneManager.LoadScene("MainMenu");
+    }
 	public void QuitGame()
 	{
 		Debug.Log ("Quitting Game");
