8940254adc1416b1a8b934f74d048f4cb0aac53a
puffisbre <puffis.stenersjo@gmail.com>
2018-11-22 16:12:01 +0100

    SomeFixed

---
+++ Björnes-Magasin-Revengeance/Assets/SpawnHere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHere : MonoBehaviour {
+
+    public GameObject spawnHere;
+    public GameObject player;
+
+
+
+   void Awake() {
+
+        spawnHere = GameObject.FindGameObjectWithTag("SpawnPoint");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        player.transform.position = spawnHere.transform.position;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+       
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
