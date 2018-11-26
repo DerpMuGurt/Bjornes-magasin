917e8f2a75bd36ab53a589df9ce1036f48933f24
Shadowsental <33833247+Shadowsental@users.noreply.github.com>
2018-11-26 10:55:14 +0100

    colors

--- Björnes-Magasin-Revengeance/Assets/Mattias/Scripts(Mattias)/DayNightScript.cs
+++ Björnes-Magasin-Revengeance/Assets/Mattias/Scripts(Mattias)/DayNightScript.cs
@@ -8,19 +8,24 @@ public class DayNightScript : MonoBehaviour
 
     public Text text;
     public Text dayText;
+    public Text timeText;
     public Text clockText;
     public Light dayLight;
-    public Light streetLight;
     public float timer = 0;
     public int days;
     int weekNumber;
     int maxDays = 32;
     int months;
-    Color orange;
-    Color darkOrange;
-    Color brightYellow;
-    Color dayColor = Color.white;
-    Color nightColor = Color.black;
+    public Color eveningColor = new Color32(236, 117, 72, 255);
+    public Color morningColor = new Color32(255, 102, 179, 255);
+    public Color dayColor = new Color32(255, 255, 255, 255);
+    public Color nightColor = new Color32(0, 0, 128, 255);
+    public Color dayTrans = new Color32(245, 198, 112, 255);
+    public Color eveningTrans = new Color32(221, 66, 129, 255);
+    public Color nightTrans = new Color32(166, 73, 142, 255);
+    public Color morningTrans = new Color32(255, 192, 203, 255);
+    public Color currentColor;
+    public bool analog;
 
     public enum Weekdays //enumerates weekdays
     {
@@ -49,14 +54,10 @@ public class DayNightScript : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
         text = text.GetComponent<Text>();
         dayText = dayText.GetComponent<Text>();
+        timeText = timeText.GetComponent<Text>();
         dayLight = dayLight.GetComponent<Light>();
-        streetLight = streetLight.GetComponent<Light>();
-        clockText = clockText.GetComponent<Text>();
-        orange = new Color32(255, 135, 0, 255);
-        darkOrange = new Color32(255, 30, 0, 255);
-        brightYellow = new Color32(255, 244, 214, 255);
         Debug.Log(timeOfDay);
-        timer = 160;
+        timer = 210;
         days = 1;
         months = 2;
         weekNumber = 1;
@@ -101,34 +102,53 @@ public class DayNightScript : MonoBehaviour
         int seconds = (int)timer % 60; //clock that displays seconds
         {
             dayText.text = ("Day ") + days.ToString();
+            timeText.text = timeOfDay.ToString();
             clockText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
             text.text = weekDays.ToString();
+            currentColor = dayLight.color;
         }
-        if (timer >= 180)
-        {
-            timeOfDay = TimeOfDay.Day;
-            dayLight.color = brightYellow;
-            Debug.Log("It's day");
-        }
-        if (timer >= 360)
-        {
-            timeOfDay = TimeOfDay.Evening;
-            dayLight.color = orange;
-            Debug.Log("It's evening");
-        }
-        if (timer >= 540)
+        if (timer >= 0)
         {
             timeOfDay = TimeOfDay.Night;
             dayLight.color = nightColor;
-            streetLight.gameObject.SetActive(true);
-            Debug.Log("It's night");
         }
-        if (timer <= 179)
+
+        if (timer >= 120)
+        {
+            dayLight.color = nightTrans;
+        }
+
+        if (timer >= 210)
         {
             timeOfDay = TimeOfDay.Morning;
-            dayLight.color = darkOrange;
-            streetLight.gameObject.SetActive(false);
-            Debug.Log("It's morning");
+            dayLight.color = morningColor;
+        }
+
+        if (timer >= 300)
+        {
+            dayLight.color = morningTrans;
+        }
+
+        if (timer >= 390)
+        {
+            timeOfDay = TimeOfDay.Day;
+            dayLight.color = dayColor;
+        }
+
+        if (timer >= 480)
+        {
+            dayLight.color = dayTrans;
+        }
+
+        if (timer >= 570)
+        {
+            timeOfDay = TimeOfDay.Evening;
+            dayLight.color = eveningColor;
+        }
+
+        if (timer >= 660)
+        {
+            dayLight.color = eveningTrans;
         }
 
         if (timer >= 720)
