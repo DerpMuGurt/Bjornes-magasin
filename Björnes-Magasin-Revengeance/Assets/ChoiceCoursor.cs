54a80011c1c6bd6be44e3ce28ab283eed143f5c0
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-11-30 16:46:00 +0100

    dd

---
+++ Björnes-Magasin-Revengeance/Assets/ChoiceCoursor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ChoiceCoursor : MonoBehaviour
+{
+
+    public GameObject coursor;
+
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        coursor.SetActive(true);
+        Debug.Log(this.gameObject.name + " was selected");
+    }
+    public void OnDeselect(BaseEventData eventData)
+    {
+        coursor.SetActive(false);
+
+    }
+}
