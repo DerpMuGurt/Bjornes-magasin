344ee37cae628b6ad47b05f3cbe541d9f8906dee
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-04 16:33:41 +0100

    Mayor

---
+++ Björnes-Magasin-Revengeance/Assets/bookScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookScript : MonoBehaviour {
+
+    public GameObject textBefore;
+    public GameObject thisObject;
+    public GameObject textAfter;
+
+	// Use this for initialization
+	void Start () {
+
+           textBefore.SetActive(false);
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E) && DialogueTrigger.talking == true || Input.GetKeyDown("joystick button 0"))
+        {
+            thisObject.SetActive(false);
+            textAfter.SetActive(true);
+        }
+    }
+}
