7945503c5e4fc359e7d72dc319f2ac9a65722893
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-06 16:46:28 +0100

    dd

--- Björnes-Magasin-Revengeance/Assets/WilliamScripts/voice.cs
+++ Björnes-Magasin-Revengeance/Assets/WilliamScripts/voice.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class voice : MonoBehaviour
 {
     public AudioClip[] voiceClipList;
+    public AudioSource voicePitch;
 
     // Use this for initialization
     void Start()
     {
         DialogueManager.voiceClipListDialogue = voiceClipList;
+        DialogueManager.voiceSource = voicePitch;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (DialogueManager.talking == true)
+        {
+            voicePitch.mute = false;
+        }
+        if (DialogueManager.talking == false)
+        {
+            voicePitch.mute = true;
+        }
     }
 }
