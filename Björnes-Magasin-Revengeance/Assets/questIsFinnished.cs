021504f125bd0755f6e3cc98f4a4460c46184261
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-07 13:12:35 +0100

    xd

---
+++ Björnes-Magasin-Revengeance/Assets/questIsFinnished.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class questIsFinnished : MonoBehaviour
+{
+
+    GameObject oldHouse;
+    GameObject newHouse;
+
+    // Use this for initialization
+    void Start()
+    {
+        oldHouse.SetActive(false);
+        newHouse.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldHouse = GameObject.FindGameObjectWithTag("BrokenBakery");
+        newHouse = GameObject.FindGameObjectWithTag("FixedBakery");
+    }
+}
