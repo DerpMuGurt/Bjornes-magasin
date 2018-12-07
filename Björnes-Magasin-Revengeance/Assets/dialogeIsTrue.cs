b5e3a811a00a8c0d4d8193e81f97ca1be47a7e2f
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-07 10:03:04 +0100

    dd

---
+++ Björnes-Magasin-Revengeance/Assets/dialogeIsTrue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogeIsTrue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        DialogueManager.talking = true;
+        FindObjectOfType<talkCamera>().LookAt();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
