780df6e320792247c34d61f18d0aabdc56354d7a
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-11 16:15:40 +0100

    fixes

--- Björnes-Magasin-Revengeance/Assets/cutScene.cs
+++ Björnes-Magasin-Revengeance/Assets/cutScene.cs
@@ -6,11 +6,15 @@ public class cutScene : MonoBehaviour
 {
 
     public GameObject Cam1;
+    bool hasPlayed;
 
     // Use this for initialization
     void Start()
     {
-        StartCoroutine(TheSequence());
+        if(hasPlayed == false)
+        {
+            StartCoroutine(TheSequence());
+        }
     }
 
     void Update()
@@ -19,8 +23,11 @@ public class cutScene : MonoBehaviour
 
     IEnumerator TheSequence()
     {
+        FindObjectOfType<Movement>().enabled = false;
         Cam1.SetActive(true);
         yield return new WaitForSeconds(4);
         Cam1.SetActive(false);
+        FindObjectOfType<Movement>().enabled = true;
+        hasPlayed = true;
     }
 }
