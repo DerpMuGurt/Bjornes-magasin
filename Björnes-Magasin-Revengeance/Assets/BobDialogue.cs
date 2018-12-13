2917e91a9005584b7132da366318e483e7fa2459
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-13 10:33:47 +0100

    dd

---
+++ Björnes-Magasin-Revengeance/Assets/BobDialogue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobDialogue : MonoBehaviour
+{
+
+    public GameObject oldDialogue;
+    public GameObject newDialogue;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+
+    }
+    // Use this for initialization
+    void Update()
+    {
+        if(questIsFinnished.questIsDone == true)
+        {
+            oldDialogue.SetActive(false);
+            newDialogue.SetActive(true);
+        }
+        if (questIsFinnished.questIsDone == false)
+        {
+            oldDialogue.SetActive(true);
+            newDialogue.SetActive(false);
+        }
+
+    }
+}
--- Björnes-Magasin-Revengeance/Assets/questIsFinnished.cs
+++ Björnes-Magasin-Revengeance/Assets/questIsFinnished.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class questIsFinnished : MonoBehaviour
 {
-
+    public static bool questIsDone;
     // Use this for initialization
-    void Start()
+    void OnEnable()
     {
-        GlobalControl.switchHouse = true;
+        questIsDone = true;
     }
 
     // Update is called once per frame
