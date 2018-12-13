81142b2067152043ed48662d1906584842db9600
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-13 11:24:08 +0100

    dd

---
+++ Björnes-Magasin-Revengeance/Assets/JimBool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JimBool : MonoBehaviour {
+
+
+    public static bool knowJim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnEnable()
+    {
+        knowJim = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Björnes-Magasin-Revengeance/Assets/JimDialogueChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JimDialogueChange : MonoBehaviour {
+    public GameObject oldDialogue;
+    public GameObject newDialogue;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+    void OnEnable()
+    {
+        if (JimBool.knowJim = true)
+        {
+            oldDialogue.SetActive(false);
+            newDialogue.SetActive(true);
+}
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
