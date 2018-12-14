e4982fea1d2e58fce9734011cb7346442d372493
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-14 14:26:33 +0100

    dd

---
+++ Björnes-Magasin-Revengeance/Assets/setActiveWindScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActiveWindScene : MonoBehaviour {
+
+    public GameObject CutSceneWin;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    OnEnabled()
+    {
+        CutSceneWin.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
