ad27ec16eeeaff1e3e9fb43b8d7199ed2aec353b
DerpMuGurt <33836944+DerpMuGurt@users.noreply.github.com>
2018-12-14 13:22:09 +0100

    Mayor fix

---
+++ Björnes-Magasin-Revengeance/Assets/MayorTotorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorTotorial : MonoBehaviour {
+
+    public Dialogue dialogue;
+    public static bool talking;
+    public AudioSource stuff;
+    public GameObject thisObject;
+
+    // Use this for initialization
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && dialogue.startDirect == true && talking == false)
+        {
+            stuff.enabled = false;
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+            DialogueTrigger.talking = true;
+            talking = true;
+        }
+        if (DialogueManager.doneTalking == true)
+        {
+            thisObject.SetActive(false);
+            stuff.enabled = true;
+            talking = false;
+        }
+    }
+}
---
+++ Björnes-Magasin-Revengeance/Assets/SetGameObjectInactive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameObjectInactive : MonoBehaviour {
+
+    public GameObject thisCon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerStay(Collider other)
+    {
+        thisCon.SetActive(false);
+    }
+        void OnDesable()
+    {
+
+    }
+}
